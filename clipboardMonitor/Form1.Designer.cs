67ec1e3a6dbd7fd0900da75552dbffdcdf979348
Oliver Warden <oliverwarden@gmail.com>
2012-09-03 22:29:16 +0100

    Initial commit

---
+++ clipboardMonitor/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace clipboardMonitor
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            ChangeClipboardChain(this.Handle, nextClipboardViewer); //remove window from chain when closed
+            mainWindow.db.Close(); //close database
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeMenu = new System.Windows.Forms.TreeView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.formatSelector = new System.Windows.Forms.ComboBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(478, 461);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeMenu);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Panel2.Controls.Add(this.formatSelector);
+            this.splitContainer1.Panel2.Controls.Add(this.resultTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(760, 541);
+            this.splitContainer1.SplitterDistance = 252;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // treeMenu
+            // 
+            this.treeMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeMenu.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeMenu.Location = new System.Drawing.Point(0, 34);
+            this.treeMenu.Name = "treeMenu";
+            this.treeMenu.Size = new System.Drawing.Size(249, 506);
+            this.treeMenu.TabIndex = 0;
+            this.treeMenu.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeMenu_AfterSelect);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(4, 40);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(485, 496);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // formatSelector
+            // 
+            this.formatSelector.FormattingEnabled = true;
+            this.formatSelector.Location = new System.Drawing.Point(11, 44);
+            this.formatSelector.Name = "formatSelector";
+            this.formatSelector.Size = new System.Drawing.Size(168, 21);
+            this.formatSelector.TabIndex = 6;
+            this.formatSelector.SelectedValueChanged += new System.EventHandler(this.formatSelector_SelectedValueChanged);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.Location = new System.Drawing.Point(4, 34);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(484, 502);
+            this.resultTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clipboard data captures";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Data for selected capture";
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 565);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "mainWindow";
+            this.Text = "ClipMon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeMenu;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.ComboBox formatSelector;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ clipboardMonitor/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Data.SQLite;
+using System.IO;
+
+namespace clipboardMonitor
+{
+    public partial class mainWindow : Form
+    {
+        [DllImport("User32.dll")]
+		    protected static extern int SetClipboardViewer(int hWndNewViewer);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        IntPtr nextClipboardViewer; //holds first window in clipboard chain
+        public static SQLiteConnection db;
+        public mainWindow()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int) this.Handle); //gets next in chain
+
+            //setup database connection
+            db = new SQLiteConnection(@"Data Source=clipDB.s3db");
+            db.Open();
+
+            populateTreeMenu();
+        }
+        protected void newClipBoardData()
+        {
+            if (db.State == System.Data.ConnectionState.Open)
+            {
+                //insert capture record
+                SQLiteCommand insertCmd = new SQLiteCommand(db);
+                insertCmd.CommandText = @"INSERT INTO capture (timestamp) VALUES('"+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")+"')";
+                int rowsUpdated = insertCmd.ExecuteNonQuery();
+                
+                insertCmd.CommandText = @"SELECT last_insert_rowid()"; //get id from capture record to use as foreign key
+                long captureID = (long) insertCmd.ExecuteScalar();
+
+                IDataObject clipData = Clipboard.GetDataObject();
+                string[] formats = clipData.GetFormats();
+                if (clipData.GetData(DataFormats.Html) != null)
+                {
+                    string html = (string)Clipboard.GetData(@"text\HTML");
+                    if (html != null)
+                    {
+                        insertCmd.CommandText = @"INSERT INTO htmlData (captureID, text) VALUES('" + captureID + "', (@html))";
+                        insertCmd.Parameters.Add("@html", DbType.String, html.Length).Value = html;
+                        insertCmd.ExecuteNonQuery();
+                    }
+                }
+                if (Clipboard.ContainsText())//insert text data
+                {
+                    insertCmd.CommandText = @"INSERT INTO textData (captureID, text) VALUES('" + captureID + "', '" + Clipboard.GetText().Replace("'", "''") + "')";
+                    rowsUpdated = insertCmd.ExecuteNonQuery();
+                }
+                if (Clipboard.ContainsImage())
+                {
+                    System.IO.MemoryStream memStream = new System.IO.MemoryStream() ;
+                    Clipboard.GetImage().Save(memStream, System.Drawing.Imaging.ImageFormat.Jpeg); //save image into stream
+                    byte[] imgData = new byte[memStream.Length];
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    memStream.Read(imgData, 0, (int) memStream.Length); //write stream onto imgData
+                    //insertCmd.CommandText = @"INSERT INTO imageData (captureID, image) VALUES('" +captureID + "', '";// + imgData + "')";
+                    insertCmd.CommandText = @"INSERT INTO imageData (captureID, image) VALUES('" +captureID + "', (@image))";
+
+
+                    //Writes to file for testing
+                    //FileStream fs = File.OpenWrite("toSQL.jpg");
+                    //fs.Write(imgData, 0, imgData.Length);
+                    //fs.Close();
+                    //
+
+
+                    //for (int i = 0; i < imgData.Length; i++)
+                    //    insertCmd.CommandText += imgData[i]; //adds image data to command
+                    insertCmd.Parameters.Add("@image", DbType.Binary, imgData.Length).Value = imgData;
+                    //insertCmd.CommandText += "')";
+                    rowsUpdated = insertCmd.ExecuteNonQuery();
+                }
+            }
+            populateTreeMenu();
+        }
+        protected override void WndProc(ref System.Windows.Forms.Message m) //override wndProc
+        {
+            //as defined in winuser.h, these are the messages we want to receive
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD: //if clipboard contents change
+                    //textBox1.Text = Clipboard.GetText();
+                    newClipBoardData(); //process new CB
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam); //pass CB data to next in chain
+                    break;
+
+                case WM_CHANGECBCHAIN: //if clipboard chain changes
+                    if (m.WParam == nextClipboardViewer) //check if window being removed is next in chain
+                        nextClipboardViewer = m.LParam; //update next in chain
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam); //pass message along chain
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        protected void populateTreeMenu()
+        {
+            treeMenu.Nodes.Clear(); //clear all nodes for an update
+            //List<TreeNode> nodes = new List<TreeNode>();
+            SQLiteCommand selectCmd = new SQLiteCommand(db);
+            selectCmd.CommandText = @"SELECT captureID, timestamp FROM capture";
+            SQLiteDataReader sqlReader = selectCmd.ExecuteReader();
+            while(sqlReader.Read())
+            {
+                TreeNode newNode = new TreeNode(sqlReader.GetString(1));
+                newNode.Tag = (int)sqlReader.GetInt32(0);
+                treeMenu.Nodes.Add(newNode);
+            }
+        }
+        /*private void button2_Click(object sender, EventArgs e) //gets image for entered captureID
+        {
+            SQLiteCommand selectCmd = new SQLiteCommand(db);
+            selectCmd.CommandText = @"SELECT image FROM imageData WHERE captureID = '" + imageIDbox.Text + "'";
+            byte[] imgData = (System.Byte[])selectCmd.ExecuteScalar();
+            //byte[] imgData = Convert.FromBase64String(imgString);
+            System.IO.MemoryStream memStream = new System.IO.MemoryStream(imgData); 
+            memStream.Write(imgData, 0, imgData.Length); //puts image data into stream
+            try { 
+                pictureBox1.Image = Image.FromStream(memStream); 
+            }
+            catch(System.ArgumentException){ 
+                Console.WriteLine("Invalid image!");
+            }
+        }*/
+        
+        void formatSelector_SelectedValueChanged(object sender, System.EventArgs e)
+        {
+            SQLiteCommand selectCmd = new SQLiteCommand(db);
+            switch ((string) formatSelector.Text)
+            {
+                case "Text":
+                    selectCmd.CommandText = @"SELECT text FROM textData WHERE captureID = '" + treeMenu.SelectedNode.Tag + "'";
+                    resultTextBox.Text = (string)selectCmd.ExecuteScalar();
+                    resultTextBox.BringToFront();
+                    break;
+                case "HTML":
+                    selectCmd.CommandText = @"SELECT text FROM htmlData WHERE captureID = '" + treeMenu.SelectedNode.Tag + "'";
+                    resultTextBox.Text = (string)selectCmd.ExecuteScalar();
+                    resultTextBox.BringToFront();
+                    break;
+                case "Render HTML":
+                    selectCmd.CommandText = @"SELECT text FROM htmlData WHERE captureID = '" + treeMenu.SelectedNode.Tag + "'";
+                    string html = (string)selectCmd.ExecuteScalar();
+                    webBrowser1.DocumentText = html;
+                    webBrowser1.BringToFront();
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized value on combo box");
+                    break;
+
+            }
+        }
+        private void treeMenu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            SQLiteCommand selectCmd = new SQLiteCommand(db);
+            formatSelector.Visible = false;
+
+            //show text
+            selectCmd.CommandText = @"SELECT text FROM textData WHERE captureID = '" + e.Node.Tag +"'";
+            resultTextBox.Text = (string) selectCmd.ExecuteScalar();
+            resultTextBox.BringToFront();
+            
+            //Add html option
+            selectCmd.CommandText = @"SELECT text FROM htmlData WHERE captureID = '" + e.Node.Tag + "'";
+            string html = (string) selectCmd.ExecuteScalar();
+            if (html != null)
+            {
+                formatSelector.Visible = true;
+                formatSelector.Text = "Text";
+                formatSelector.Items.Clear();
+                formatSelector.Items.Add("Text");
+                formatSelector.Items.Add("HTML");
+                formatSelector.Items.Add("Render HTML");
+            }
+
+
+            //show image
+            selectCmd.CommandText = @"SELECT image FROM imageData WHERE captureID = '" + e.Node.Tag + "'";
+            byte[] imgData = (System.Byte[])selectCmd.ExecuteScalar();
+            //byte[] imgData = Convert.FromBase64String(imgString);
+            if (imgData != null) //only if image found
+            {
+                System.IO.MemoryStream memStream = new System.IO.MemoryStream(imgData);
+                memStream.Write(imgData, 0, imgData.Length); //puts image data into stream
+                try
+                {
+                    pictureBox1.Image = Image.FromStream(memStream);
+                    pictureBox1.BringToFront();
+                }
+                catch (System.ArgumentException)
+                {
+                    Console.WriteLine("Invalid image!");
+                }
+            }
+        }
+    }
+}
